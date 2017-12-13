0c96d525cbd9ee22397c62e1138575f3a9667560
Vartok Soma <vartok.soma@gmail.com>
2017-12-13 10:26:41 +0100

    tried my best to finish as much of it as i could...

--- server.py
+++ server.py
@@ -3,16 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def route_home():
-
     return render_template('home.html')
 
 
 @app.route('/request-counter', methods=['GET', 'POST'])
 def route_request_counter():
-    counts = {GET:0, POST:0}
+    counts = {GET: 0, POST: 0}
     global counts
     if methods == 'GET':
         counts[GET] += 1
@@ -21,9 +19,11 @@ def route_request_counter():
         counts[POST] += 1
     return render_template('request_counter.html')
 
+
 @app.route('/statistics')
 def route_statistics():
     pass
+
 
 if __name__ == '__main__':
     app.run(
